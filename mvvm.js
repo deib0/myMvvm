3817e430f9961433f5e947d3bd1a37661aa29b4e
deibo <deib0@foxmail.com>
2022-09-11 22:29:36 +0800

    完成数据劫持observe函数

--- mvvm.js
+++ mvvm.js
@@ -1,3 +1,30 @@
-window.Mvvm=function(){
-
+function observe(data={}){
+    // this.options.data 如果不是对象则返回
+    if(typeof data !== 'object')return
+    for(let key in data){
+        let val =data[key]
+        if(typeof val === 'object')observe(val)
+        Object.defineProperty(data,key,{
+            configurable:true,
+            get(){return val},
+            set(newVal){
+                if(val===newVal)return
+                val =newVal
+                observe(newVal)
+                console.log('数据更新了')
+            }
+        })    
+    }
 }
+
+function Mvvm(options={}){
+    // 将 options 挂载到实例上的 $options
+    this.$options=options
+    // 等同于 let data = this.$options.data;this._data=this.$options.data
+    let data = this._data=this.$options.data
+    // 数据劫持
+    observe(data)
+}
+
+
+
