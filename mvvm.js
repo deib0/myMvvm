4d25c416121240db3d416fd76a46e1a7cde70205
deibo <deib0@foxmail.com>
2022-09-12 10:37:31 +0800

    完成数据编译函数compile

--- mvvm.js
+++ mvvm.js
@@ -16,18 +16,57 @@ function observe(data={}){
         })    
     }
 }
-function proxy(data){
+function proxy(data,vm){
     for(let key in data){
-        Object.defineProperty(this,key,{
+        Object.defineProperty(vm,key,{
             configurable:true,
             get(){
-                return this._data[key]
+                return vm._data[key]
             },
             set(newVal){
-                this._data[key]=newVal
+                vm._data[key]=newVal
             }
         })
     }
+}
+// 创建compile函数
+function compile(el, vm) {
+    // 将el挂载到实例上方便调用
+    vm.$el = document.querySelector(el);
+    // fragment 不在 DOM 树中，不会引起回流，更佳的性能
+    let fragment = document.createDocumentFragment();
+    while (child = vm.$el.firstChild) {
+        fragment.appendChild(child);    // 此时将el中的内容放入内存中
+    }
+    // 对el里面的内容进行替换
+    function replace(frag) {
+        Array.from(frag.childNodes).forEach(node => {
+            let txt = node.textContent;
+            let reg = /\{\{(.*?)\}\}/g;   // 正则匹配{{}}
+            // 即是文本节点又有大括号的情况{{}}
+            if (node.nodeType === 3 && reg.test(txt)) { 
+                let result ={}
+                let matchStr=txt.match(reg)
+                // 处理字符串
+                matchStr.forEach(item=>{
+                    let formatStr =item.slice(2).slice(0,-2).split('.')
+                    let val=vm
+                    formatStr.forEach(item=>val=val[item])
+                    result[item]=val
+                })
+                for(let key in result){
+                    txt=txt.replace(key, result[key]).trim()
+                }
+                node.textContent=txt
+            }
+            // 如果还有子节点，继续递归replace
+            if (node.childNodes && node.childNodes.length) {
+                replace(node);
+            }
+        });
+    }
+    replace(fragment);  // 替换内容
+    vm.$el.appendChild(fragment);   // 再将文档碎片放入el中
 }
 
 function Mvvm(options={}){
@@ -38,7 +77,9 @@ function Mvvm(options={}){
     // 数据劫持
     observe(data)
     // 数据代理
-    proxy.call(this,data)
+    proxy(data,this)
+    // 编译    
++   compile(options.el, this);
 
 }
 
